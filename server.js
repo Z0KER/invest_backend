eb05731b25436295a5c95ecf997307ed3596d885
flavio <Flvio>
2022-12-16 17:11:37 -0300

    procfile

--- server.js
+++ server.js
@@ -41,5 +41,5 @@ app.get('/download', (req, res) => {
 })
 
 app.listen(process.env.PORT || 8080, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+    console.log('Server Running!')
 });
